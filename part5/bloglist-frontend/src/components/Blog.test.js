6ae34119e33ce0e659ae90c29a774d5875af23ad
ElenaSj <elena.sjoblom@gmail.com>
2023-09-21 13:53:00 +0300

    part5 exercise 5.15

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -61,3 +61,36 @@ test('renders url and number of likes when button is clicked', async () => {
   screen.findByText('www.kavioliitto.fi')
   screen.findByText('3')
 })
+
+test('when like button is clicked twice, event handler is called twice', async () => {
+  const blog = {
+    'title': 'Kavioliitossa',
+    'author': 'Katja Ståhl',
+    'url': 'www.kavioliitto.fi',
+    'likes': 3,
+    'user': {
+      'username': 'test',
+      'name': 'Reija Hyyppälä-Kaskela',
+      'id': '6451fdbe7de67a49e63499ea'
+    },
+    'id': '64f8561f4a3890bf3eaf2932'
+  }
+
+  const user1 = {
+    'username': 'test',
+    'name': 'Reija Hyyppälä-Kaskela',
+    'id': '6451fdbe7de67a49e63499ea'
+  }
+
+  const eventHandler = jest.fn()
+
+  render(<Blog blog={blog} user = {user1} updateBlog={eventHandler} />)
+
+  const user = userEvent.setup()
+  const viewbutton = screen.getByText('view')
+  await user.click(viewbutton)
+  const likebutton = screen.getByText('like')
+  await user.click(likebutton)
+  await user.click(likebutton)
+  expect(eventHandler).toHaveBeenCalledTimes(2)
+})
