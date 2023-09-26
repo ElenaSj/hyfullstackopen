e41e6960ea12fed33015b89da9d03cee0a49f80b
ElenaSj <elena.sjoblom@gmail.com>
2023-09-26 11:32:10 +0300

    part5 exercise 5.18

--- part5/bloglist-frontend/cypress/e2e/bloglist-app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/bloglist-app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Testi Käyttäjä',
+      username: 'testuser',
+      password: 'secret'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:5173')
   })
 
@@ -8,5 +14,23 @@ describe('Blog app', function() {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('input:first').type('testuser')
+      cy.get('input:last').type('secret')
+      cy.contains('login').click()
+      cy.contains('Testi Käyttäjä is logged in')
+    })
+
+    it('fails with wring credentials', function() {
+      cy.get('input:first').type('test')
+      cy.get('input:last').type('test')
+      cy.contains('login').click()
+      cy.get('.error')
+        .should('contain', 'Wrong credentials')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
